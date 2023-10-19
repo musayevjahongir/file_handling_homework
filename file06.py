323690a4b85609d767852f417f39878be0045326
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-19 15:23:37 +0500

    qatorlar uzunligi

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f=open(data, mode="r")
+    a=f.readlines()
+    l=[]
+    for i in a:
+        l.append(len(i)-1)
+    l[-1]+=1
+    return l
+print(main("data/data06.txt"))
 # Read data from file
