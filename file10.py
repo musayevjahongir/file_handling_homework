be8c466f5d75ec408cc57664e1a79e17d9b9b17f
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-19 15:46:36 +0500

    eng uzun qator

--- file10.py
+++ file10.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f=open(data, mode="r")
+    a=f.readlines()
+    l=[]
+    for i in a:
+        l.append(len(i)-1)
+    l[-1]+=1
+    return max(l)
+print(main("data/data10.txt"))
 # Read data from file
