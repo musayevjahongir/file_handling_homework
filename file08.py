f0f248c89648b32f1093dc433d373cc98137331b
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-19 15:41:01 +0500

    katta raqam

--- file08.py
+++ file08.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    a=[]
+    f=open(data).read()
+    for i in f:
+        if i.isdigit():
+            a.append(i)
+    return max(a)
+print(main("data/data08.txt"))
 # Read data from file
